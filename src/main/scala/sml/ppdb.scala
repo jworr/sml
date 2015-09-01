91da2ee3f40818bca23bb7884caf3baa5171638f
Walker Orr <walker.orr@gmail.com>
2015-08-31 20:16:56 -0700

    added traversers for ppdb

--- src/main/scala/sml/ppdb.scala
+++ src/main/scala/sml/ppdb.scala
@@ -1,6 +1,7 @@
 package sml
 
 import java.sql.Connection
+import scala.collection.mutable.{HashSet, Queue}
 
 import sml.sqlite.{iterableResults, prepareAndQuery}
 
@@ -15,7 +16,7 @@ object ppdb
 	/**
 	Returns true if the phrase is in the database
 	*/
-	def hasPhrase(db:Connection, phrase:String): Boolean =
+	def hasPhrase(db:Connection)(phrase:String): Boolean =
 	{
 		val sql = "select count(*) from similarity where source = ?"
 
@@ -32,7 +33,7 @@ object ppdb
 	/**
 	Returns all similar phrases to the given phrase along with a similarity score
 	*/
-	def similarPhrases(db:Connection, phrase:String, agiga:Boolean = true): Iterable[(String, Double)] =
+	def similarPhrases(db:Connection)(phrase:String, agiga:Boolean = true): Iterable[(String, Double)] =
 	{
 		val query = if(agiga) 
 			"select target, agiga_sim, pos from similarity where source = ?"
@@ -49,13 +50,137 @@ object ppdb
 		}
 	}
 
+	/**
+	Returns only those phrases which are unique neighbors of the given phrase
+	*/
+	def neighbors(db:Connection, phrase:String): Iterable[String] =
+	{
+		val query = "select distinct target from similarity where source = ?"
+	
+		//execute the query and return the results
+		return prepareAndQuery(db, query, phrase.toLowerCase).map(_.getString(1))
+	}
+
+	/**
+	Returns the similarity of two phrases base on the overlap in their neighbors
+	*/
+	def adjSim(db:Connection, left:String, right:String): Double =
+	{
+		val leftNeig = neighbors(db, left).toSet
+		val rightNeig = neighbors(db, right).toSet
+		val unionSize = leftNeig.union(rightNeig).size.toDouble
+	
+		if(unionSize > 0.0)
+			leftNeig.intersect(rightNeig).size.toDouble / unionSize
+		else
+			0.0
+	}
+
+	/**
+	A phrase in PPDB
+	*/
+	class PPDBNode(val phrase:String, val score:Double)
+	{
+		override def equals(other:Any): Boolean = other match
+		{
+			case that:PPDBNode => that.phrase == phrase
+			case _ => false
+		}
+		
+		override def hashCode:Int = phrase.hashCode
+	}
+
+	/**
+	Defines a traverser over PPDB
+	*/
+	abstract class Traverser(val db:Connection, val start:String) extends Iterable[String]
+	{
+		/**
+		Wraps a paraphrase in a node
+		*/
+		implicit def init( phrase:String ): PPDBNode
+		
+		/**
+		Returns a nodes successors
+		*/
+		def successors(node:PPDBNode):Iterable[PPDBNode]
+
+		/**
+		Returns an iterator over phrase
+		*/
+		def iterator = new Iterator[String]
+		{
+			val visited = new HashSet[PPDBNode]()
+			val fringe = new Queue[PPDBNode]() ++ List[PPDBNode](start)
+
+			override def hasNext:Boolean = !fringe.isEmpty
+		
+			override def next:String = 
+			{
+				//get the current
+				val current = fringe.dequeue
+
+				//mark it as visited
+				visited += current
+
+				//add its children to the fringe
+				fringe ++= (successors(current).filter(n => !visited(n)))
+			
+				//return the current value
+				return current.phrase
+			}
+		}
+	}
+
+	/**
+	Traverses PPDB based on distance from the starting point
+	*/
+	case class DistTraverser(startNode:String, val limit:Int, conn:Connection) extends Traverser(conn, startNode)
+	{
+		implicit def init(phrase:String) = new PPDBNode(phrase,0.0)
+
+		/**
+		Returns all nodes one step away from the current node
+		*/
+		def successors(node:PPDBNode):Iterable[PPDBNode] =
+		{
+			val newScore = node.score + 1
+			
+			//get the neighors we are not beyond our limit
+			if(newScore <= limit)
+				neighbors(db, node.phrase).map(s => new PPDBNode(s, newScore))
+			else
+				List[PPDBNode]()
+		}
+	}
+
+	/**
+	Traverses PPDB based on similarity
+	*/
+	case class SimTraverser(startNode:String, val limit:Double, conn:Connection) extends Traverser(conn, startNode)
+	{
+		implicit def init(phrase:String) = new PPDBNode(phrase, 1.0)
+
+		/**
+		Returns all nodes that are within a similarity threshold of the the start
+		*/
+		def successors(node:PPDBNode):Iterable[PPDBNode] =
+		{
+			neighbors(db, node.phrase).map(s => new PPDBNode(s, node.score * adjSim(db, s, node.phrase))).filter(_.score >= limit)
+		}
+	}
+
+	def distTraverse(limit:Int, db:Connection)(start:String):Iterable[String] = DistTraverser(start,limit,db).toSeq
+
+	def simTraverse(limit:Int, db:Connection)(start:String):Iterable[String] = SimTraverser(start,limit,db).toSeq
+
 	def main(args:Array[String])
 	{
 		val db = connect("/home/walker/Data/ppdb/small_no_num.db")
 
-		for( result <- similarPhrases(db, "arrested") )
+		for( result <- distTraverse(5, db)("arrested") )
 		{
-			println(result._1)
+			println(result)
 		}
 	}
 }
