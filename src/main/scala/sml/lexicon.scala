531b9666c174aedce49727385503c667d93a434d
Walker Orr <walker.orr@gmail.com>
2015-09-22 09:33:45 -0700

    added sparse bag of words data structure

--- src/main/scala/sml/lexicon.scala
+++ src/main/scala/sml/lexicon.scala
@@ -1,6 +1,6 @@
 package sml
 
-import sml.learn.LabeledInstance
+import sml.learn.{Instance,LabeledInstance}
 
 /**
 A sparse bag-of-words style collection
@@ -10,19 +10,49 @@ object lexicon
 	/**
 	A sparse collection of strings with a binary variable interface
 	*/
-	class Lexicon[C](source:Seq[Iterable[String]], val domain:Set[String], labels:Seq[C]) extends Iterable[LabeledInstance[C]]
+	class Lexicon[C](source:Seq[Iterable[String]], val domain:Dictionary, labels:Seq[C]) 
+		extends Iterable[LabeledInstance[C]]
 	{
-		val wordToIndex = domain.zipWithIndex.toMap
-		val data = source.zip(labels).map(p => new SparseInstance(bagToSet(p._1, wordToIndex), domain.size, p._2))
+		val data = source.zip(labels).map(p => domain.makeLabeledInstance(p._1, p._2))
 
 		def iterator:Iterator[LabeledInstance[C]] = data.iterator
 	}
 
+	/**
+	A mapping between Strings and indexes
+	*/
+	class Dictionary(val domain:Set[String])
+	{
+		val wordToIndex = domain.zipWithIndex.toMap
+
+		/**
+		Makes a sparse set out of a collection of strings
+		*/
+		def makeSparse(data:Iterable[String]):Set[Int] =
+		{
+			data.map(s => wordToIndex.getOrElse(s,-1)).filter(i => i != -1).toSet
+		}
+
+		/**
+		Creates an instance based on the dictionary's domain
+		*/
+		def makeInstance(data:Iterable[String]):SparseInstance = new SparseInstance(makeSparse(data), size)
+
+		/**
+		Creates a labeled instance based on the dictionary's domain
+		*/
+		def makeLabeledInstance[C](data:Iterable[String], label:C):LabeledSparseInstance[C] =
+		{
+			new LabeledSparseInstance(makeSparse(data), size, label)
+		}
+
+		def size:Int = domain.size
+	}
 
 	/**
 	A sparse instance
 	*/
-	class SparseInstance[C](val data:Set[Int], val domainSize:Int, val labelValue:C) extends LabeledInstance[C]
+	class SparseInstance(val data:Set[Int], val domainSize:Int) extends Instance
 	{
 		/**
 		Returns the feature value
@@ -40,16 +70,16 @@ object lexicon
 		def features:Iterable[Double] = Range(0,domainSize).map(i => featureAt(i))
 
 		def size:Int = domainSize
-
-		def label:C = labelValue
 	}
 
 	/**
-	Reprents a bag of words as a set of indexes
+	A sparse instance with a label
 	*/
-	def bagToSet(data:Iterable[String], wordMap:Map[String,Int]):Set[Int] =
+	class LabeledSparseInstance[C](data:Set[Int], domainSize:Int, val labelValue:C) 
+		extends SparseInstance(data,domainSize) 
+		with LabeledInstance[C]
 	{
-		data.map(s => wordMap.getOrElse(s,-1)).filter(i => i != -1).toSet
+		def label:C = labelValue
 	}
 
 	/**
