1faa17d48431dffbe60e29cc9cf286fe10d74a8a
Walker Orr <>
2016-01-18 12:28:13 -0800

    Added Counter class

--- src/main/scala/sml/util.scala
+++ src/main/scala/sml/util.scala
@@ -1,5 +1,7 @@
 package sml
 
+import scala.collection.mutable.HashMap
+
 /**
 Contains general untility functions
 */
@@ -9,4 +11,29 @@ object util
 	Generates and returns a time based UUID
 	*/
 	def UUID:String = java.util.UUID.randomUUID.toString
+
+	/**
+	 * A datastructure to count the number of occurances of an object i.e. a
+	 * multiset
+	 */
+	case class Counter[T](items:Iterable[T])
+	{
+		val counts = 
+		{
+			val total = new HashMap[T,Int]()
+
+			//total up the number of each item
+			for(item <- items)
+			{
+				total(item) = total.getOrElse(item, 0) + 1
+			}
+
+			total.toMap
+		}
+
+		/**
+		 * Returns the number of duplicates
+		 */
+		def duplicateCounts:Int = counts.values.sum - counts.keys.size
+	}
 }
